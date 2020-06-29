2755fd6b339a6e97fda27b46afe682dddda27a62
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-06-29 15:34:23 -0400

    admin dash edit

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Blog On Rails"
+  config.site_title = "GastroDev BlogSkeleton"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -239,11 +239,11 @@ ActiveAdmin.setup do |config|
   #
   # If you wanted to add a static menu item to the default menu provided:
   #
-  #   config.namespace :admin do |admin|
-  #     admin.build_menu :default do |menu|
-  #       menu.add label: "My Great Website", url: "http://www.mygreatwebsite.com", html_options: { target: :blank }
-  #     end
-  #   end
+    config.namespace :admin do |admin|
+      admin.build_menu :default do |menu|
+        menu.add label: "Home", url: "http://localhost:3000", html_options: { target: :blank }
+      end
+    end
 
   # == Download Links
   #
@@ -317,7 +317,7 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
+  config.footer = 'Blog Skeleton by Gastronomic Development'
 
   # == Sorting
   #
